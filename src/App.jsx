a8e848ddfd2fbf3cbd10c627377aaeba2739c860
Zaptas Technologies Pvt Ltd <96110596+kushagra-01@users.noreply.github.com>
2024-11-05 18:14:23 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "/",
+        path: "/intranetdemoFront",
         element: <Home />,
       },
       {
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-    throw new Error("Root element not found!");
-}
-
-createRoot(rootElement).render(
-    <StrictMode>
-        <BrowserRouter basename="/intranetdemoFront"> {/* Set basename here */}
-            <App />
-        </BrowserRouter>
-    </StrictMode>,
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
