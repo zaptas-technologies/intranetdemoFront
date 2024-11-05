80788a82229e03e9503a8acb6bfbb71fabcb90e9
Zaptas Technologies Pvt Ltd <96110596+kushagra-01@users.noreply.github.com>
2024-11-05 17:51:27 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  base: '/intranetdemoFront/', 
+  base: '/intranetdemoFront/', // Ensure this matches your GitHub repo name
 })
